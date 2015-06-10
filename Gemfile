eb87b8ed76b5364d20298dec2b25d31e3172f9b0
sawanoboly <sawanoboriyu@higanworks.com>
2015-06-11 00:43:57 +0900

    initial commit

---
+++ Gemfile
@@ -0,0 +1,7 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+gem 'test-kitchen', path: "../../higanworks/test-kitchen", ref: 'verify_from_local'
+gem 'kitchen-ec2'
+# gem "kitchen-vagrant"
+gem 'infrataster'
---
+++ spec/test_spec.rb
@@ -0,0 +1,24 @@
+# coding: utf-8
+require 'spec_helper'
+
+describe server(:amimoto) do
+  describe capybara('http://amimoto') do
+    it "Blocks invarid string" do
+      visit '/wp-admin/install.php'
+      fill_in "instance_id", with: 'hogehoge'
+      click_button 'Next Step'
+      expect(page).to have_content "Sorry, that isn’t a valid instance ID."
+    end
+
+    it "Unlock Success" do
+      visit '/wp-admin/install.php'
+      fill_in "instance_id", with: ENV['KITCHEN_SERVER_ID']
+      expect(click_button 'Next Step').not_to be_nil
+    end
+
+    it "Should got 'Welcome' after unlock" do
+      visit '/wp-admin/install.php'
+      expect(page).to have_content 'ようこそ'
+    end
+  end
+end
