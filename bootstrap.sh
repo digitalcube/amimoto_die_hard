e826e5ee743ab0662e5c7984d38e3b1e729d7495
sawanoboly <sawanoboriyu@higanworks.com>
2015-07-01 13:44:29 +0900

    wait for wp-config.php

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env bash
 
+## Wait Amimot setup
+I_ID=`/opt/aws/bin/ec2-metadata | grep instance-id | awk '{print $2}'`
 
-for x in {1..60}; do echo '.' ; sleep 8 ; done
+while :
+do
+echo -n "."
+sleep 5
+  if [ -f /var/www/vhosts/${I_ID}/wp-config.php ] ; then
+    echo
+    break
+  fi
+done
+
 sudo yum -y update
 
 for x in nginx hhvm php-fpm ; do
   if service $x status ; then service $x restart ; fi
 done
 
+sleep 3
