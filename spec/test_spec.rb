86e44fd6e9d970241456a7ed039d3ea6e132172b
sawanoboly <sawanoboriyu@higanworks.com>
2015-12-16 18:42:14 +0900

    [ci skip] update rule for global installation

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -18,7 +18,7 @@ describe server(:amimoto) do
 
     it "Should got 'Welcome' after unlock" do
       visit '/wp-admin/install.php'
-      expect(page).to have_content 'ようこそ'
+      expect(page).to have_content 'WordPress Select a default language'
     end
   end
 end
