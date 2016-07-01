144624abeb09be2992edfd3e395fbb4cd968a370
sawanoboly <sawanoboriyu@higanworks.com>
2016-07-01 15:28:48 +0900

    dispatch by branch name

---
+++ ci/dispatcher.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+if [ ! "true" == "$CI" ] ; then
+  bundle exec kitchen test --destroy=always --concurrency=7
+  exit $?
+fi
+
+if [ ! "master" == "$CIRCLE_BLANCH" ] ; then
+  bundle exec kitchen test --destroy=always --concurrency=7
+  exit $?
+fi
+
+bundle exec kitchen test ${CIRCLE_BLANCH} --destroy=always --concurrency=7
