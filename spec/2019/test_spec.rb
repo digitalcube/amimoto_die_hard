548a3c7afa3653700b128da48ded558dafc84a28
sawanoboly <sawanoboriyu@higanworks.com>
2021-08-24 18:52:55 +0900

    update legacy spec

--- spec/2019/test_spec.rb
+++ spec/2019/test_spec.rb
@@ -1,19 +1,16 @@
 # coding: utf-8
 require 'spec_helper'
 
-# describe server(:amimoto) do
 describe ENV['KITCHEN_HOSTNAME'] do
-  # describe capybara("http://#{ENV['KITCHEN_HOSTNAME']}") do
-    it "Logs in with instance_id" do
-      visit '/wp-login.php'
-      # pp page.driver.browser.manage.all_cookies
-      fill_in "user_login", with: 'admin'
-      fill_in "user_pass", with: ENV['KITCHEN_SERVER_ID']
-      click_button 'wp-submit'
+  it "Logs in with instance_id" do
+    visit '/wp-login.php'
+    # pp page.driver.browser.manage.all_cookies
+    fill_in "user_login", with: 'admin'
+    fill_in "user_pass", with: ENV['KITCHEN_SERVER_ID']
+    click_button 'wp-submit'
 
-      # cookie was not set..? pending
-      expect(page).to have_content 'Cookies are blocked or not supported by your browser'
-      # assert_title 'Dashboard'
-    end
+    # cookie was not set..? pending
+    expect(page).to have_content 'Cookies are blocked or not supported by your browser'
+    # assert_title 'Dashboard'
   end
-# end
+end
--- spec/legacy/test_spec.rb
+++ spec/legacy/test_spec.rb
@@ -1,24 +1,22 @@
 # coding: utf-8
 require 'spec_helper'
 
-describe server(:amimoto) do
-  describe capybara('http://amimoto') do
-    it "Blocks invalid string" do
-      visit '/wp-admin/install.php'
-      fill_in "instance_id", with: 'hogehoge'
-      click_button 'Next Step'
-      expect(page).to have_content "Sorry, that isn’t a valid instance ID."
-    end
+describe ENV['KITCHEN_HOSTNAME'] do
+  it "Blocks invalid string" do
+    visit '/wp-admin/install.php'
+    fill_in "instance_id", with: 'hogehoge'
+    click_button 'Next Step'
+    expect(page).to have_content "Sorry, that isn’t a valid instance ID."
+  end
 
-    it "Unlock Success" do
-      visit '/wp-admin/install.php'
-      fill_in "instance_id", with: ENV['KITCHEN_SERVER_ID']
-      expect(click_button 'Next Step').not_to be_nil
-    end
+  it "Unlock Success" do
+    visit '/wp-admin/install.php'
+    fill_in "instance_id", with: ENV['KITCHEN_SERVER_ID']
+    expect(click_button 'Next Step').not_to be_nil
+  end
 
-    it "Should got 'Welcome' after unlock" do
-      visit '/wp-admin/install.php'
-      expect(page).to have_content (/WordPress\nSelect a default language\nEnglish/)
-    end
+  it "Should got 'Welcome' after unlock" do
+    visit '/wp-admin/install.php'
+    expect(page).to have_content (/WordPress\nSelect a default language\nEnglish/)
   end
 end
